09b20831363837996160e08e6813d7353f5ab37f
Kjetil Bugge Kristoffersen <kjetilbk@ifi.uio.no>
2017-07-28 16:03:56 +0200

    styling changes

--- src/components/utfallovertid/Legend.jsx
+++ src/components/utfallovertid/Legend.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Legend = ({dataDescription}) => {
-  const legendElements = dataDescription.map(datapoint => <div className="lelem"><div style={{backgroundColor: datapoint.color}} className="legend-color"></div><p className="legend-name">{datapoint.name}</p></div>)
+  const legendElements = dataDescription.map(datapoint => <div key={datapoint.name.toLowerCase()} className="lelem"><div style={{backgroundColor: datapoint.color}} className="legend-color"></div><p className="legend-name">{datapoint.name}</p></div>)
   return (
     <div className="graf-legend">
       <div className="legend-elems">
